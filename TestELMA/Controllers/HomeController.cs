1724e320dd7f712053a8d8839c3182661291173e
Максим Новиков <pylliok1@mail.ru>
2019-11-08 11:55:06 +0300

    1111

---
+++ TestELMA/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TestELMA.Models;
+
+namespace TestELMA.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+            using (var client = new WebClient()) 
+            using (var stream = client.OpenRead("https://www.cbr-xml-daily.ru/daily_json.js"))
+            using (var sr = new StreamReader(stream))
+            {
+                string text = sr.ReadToEnd();
+                JObject o = JObject.Parse(text);
+                var a = o["Valute"].Children();
+                foreach (var item in a)
+                {
+                    SelectListItem item1 = new SelectListItem { Text = item.First["CharCode"].ToString(), Value = item.First["CharCode"].ToString() };
+                    selectListItems.Add(item1);
+                }
+            }
+            SelectList b = new SelectList(selectListItems, "Value", "Text");
+            ViewBag.List = b;
+            return View();
+        }
+        public ActionResult Update(string name)
+        {
+            
+            ModelView model = new ModelView();
+            using (var client = new WebClient())
+            using (var stream = client.OpenRead("https://www.cbr-xml-daily.ru/daily_json.js"))
+            using (var sr = new StreamReader(stream))
+            {
+                string text = sr.ReadToEnd();
+                JObject o = JObject.Parse(text);
+                var a = o["Valute"][name];
+                model = a.ToObject<ModelView>();
+            }
+            return PartialView(model);
+        }
+    }
+}
---
+++ TestELMA/Models/ModelView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestELMA.Models
+{
+    public class ModelView
+    {
+        public string ID { get; set; }
+        [Display(Name = "Цифр. код")]
+        public string NumCode { get; set; }
+        [Display(Name = "Букв. код")]
+        public string CharCode { get; set; }
+        [Display(Name = "Единиц")]
+        public int Nominal { get; set; }
+        [Display(Name = "Валюта")]
+        public string Name { get; set; }
+        [Display(Name = "Курс")]
+        public double Value { get; set; }
+        [Display(Name = "Предыдущий курс")]
+        public double Previous { get; set; }
+    }
+}
